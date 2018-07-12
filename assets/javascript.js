e54102412d7b39e0406d5878d7360baf62f4bc21
Christopher Mueller <christopher.mueller@cpxprojects.com>
2018-07-12 13:04:38 -0400

    fixed bug issue with jQuery

--- assets/javascript.js
+++ assets/javascript.js
@@ -35,4 +35,46 @@ $("#submitButton").on("click", function (event) {
     }
 
     database.ref().push(temp);
+
+    // Storing our giphy API URL for a random cat image
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
+
+    // Perfoming an AJAX GET request to our queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+    // After the data from the AJAX request comes back
+      .then(function(response) {
+          console.log(response);
+
+      // Saving the image_original_url property
+        var imageUrl = response.data.image_original_url;
+
+        // Creating and storing an image tag
+        var catImage = $("<img>");
+
+        // Setting the catImage src attribute to imageUrl
+        catImage.attr("src", imageUrl);
+        catImage.attr("alt", "cat image");
+
+        // Prepending the catImage to the images div
+        $("#images").prepend(catImage);
+      });
+
+    
 });
+
+//grab the added information
+database.ref().on("child_added", function (childSnaphot) {
+    console.log(childSnaphot.val().first_name);
+    console.log(childSnaphot.val().last_name);
+
+    //add new rows to table
+    $("#tablebody").append($("<tr><td>"
+    +childSnaphot.val().first_name+"</td><td>"
+    +childSnaphot.val().last_name
+    +"</td></tr>"))
+  
+  });
